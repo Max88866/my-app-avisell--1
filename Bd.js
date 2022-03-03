248fd4399cbc4e829a75ac89129766a677f916ab
Max88866 <96172056+Max88866@users.noreply.github.com>
2022-03-03 20:57:08 +0700

    back

---
+++ Bd.js
@@ -0,0 +1,299 @@
+module.exports = {
+  TicketsMasiv1: {
+    stop: false,
+    tickets: [
+      {
+        price: 12523,
+        carrier: "FB",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T14:58:00.000Z",
+            stops: ["SKK", "Gh"],
+            duration: 2111,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T16:29:00.000Z",
+            stops: ["SKK", "Gh"],
+            duration: 15801,
+          },
+        ],
+      },
+      {
+        price: 20600,
+        carrier: "FV",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: [],
+            duration: 7967,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: [],
+            duration: 6600,
+          },
+        ],
+      },
+      {
+        price: 20600,
+        carrier: "FQ",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 67,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK"],
+            duration: 80,
+          },
+        ],
+      },
+      {
+        price: 300,
+        carrier: "FR",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK", "BKK", "BH"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["AK", "BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        price: 22600,
+        carrier: "FX",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: [],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: [],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        price: 212600,
+        carrier: "FS",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        price: 600,
+        carrier: "FP",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+    ],
+  },
+  TicketsMasiv2: {
+    stop: true,
+    tickets: [
+      {
+        id: 1,
+        price: 12523,
+        carrier: "FB",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T14:58:00.000Z",
+            stops: ["SKK", "Gh"],
+            duration: 2111,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T16:29:00.000Z",
+            stops: ["SKK", "Gh"],
+            duration: 15801,
+          },
+        ],
+      },
+      {
+        id: 2,
+        price: 20600,
+        carrier: "FV",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: [],
+            duration: 7967,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: [],
+            duration: 6600,
+          },
+        ],
+      },
+      {
+        id: 3,
+        price: 20600,
+        carrier: "FQ",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 67,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK"],
+            duration: 80,
+          },
+        ],
+      },
+      {
+        id: 4,
+        price: 300,
+        carrier: "FR",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK", "BKK", "BH"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["AK", "BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        id: 5,
+        price: 22600,
+        carrier: "FX",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: [],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: [],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        id: 6,
+        price: 212600,
+        carrier: "FS",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+      {
+        id: 7,
+        price: 600,
+        carrier: "FP",
+        segments: [
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-05-19T11:18:00.000Z",
+            stops: ["AK"],
+            duration: 867,
+          },
+          {
+            origin: "MOW",
+            destination: "HKT",
+            date: "2020-06-08T18:39:00.000Z",
+            stops: ["BKK", "BH"],
+            duration: 600,
+          },
+        ],
+      },
+    ],
+  },
+};
